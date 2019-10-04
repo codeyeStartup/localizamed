c9b4972ba756126665ef10192d154ce42cf1c4d5
augusto <augustosnk12@gmail.com>
2019-10-04 11:13:08 -0300

    projeto base do flutter

---
+++ localizamedapp/lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        Container(color: Colors.yellow,)
+      ],
+    );
+  }
+}
